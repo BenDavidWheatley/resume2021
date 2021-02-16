a266566769e06b446f538c5e463d92d36424d500
Ben Wheatley <Info@wheatleystudios.com>
2021-02-16 20:36:25 +0200

    CSS style established. Most of the HTML added. Rellax plugin added.

---
+++ index.php
@@ -0,0 +1,197 @@
+<?php session_start();?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="style/style.css" type="text/css">
+    <title>Ben Wheatley resume 2021</title>
+  </head>
+  <header class='headerContainer'>
+
+  </header>
+
+  <body>
+    <div id="introProfile">
+        <p   id="profileImage">placeholder</p>
+        <h1 class="rellax" data-rellax-speed="6" id="name">BEN WHEATLEY</h1>
+        <section id=aboutOverlay>
+          <div class="rellax" data-rellax-speed="4" id="mainOverlayText">
+            <h3>Full Stack Web Developer</h3>
+            <p>Enthusiastic web developer eager to contribute to team success through hard work, attention to detail and excellent organisational skills. Clear understanding of how to use HTML, CSS and Javascript to create websites. Further training in progress includes backend programming, databases and dynamic web apps. Motivated to learn, grow and excel as a full stack developer.</p>
+          </div>
+        </section>
+
+        <Section class="rellax" data-rellax-speed="10"  id="cv">
+          <div  class="line"></div>
+          <p>CURRICULUM VITEA</p>
+        </Section>
+
+        <section class="rellax" data-rellax-speed="6" id="year">
+          <h1 id="twentyOne">2021</h1>
+        </section>       
+        <section class="rellax" data-rellax-speed="10" id="signature">
+          
+        </section>   
+    </div>
+
+    <!-- page two -->
+
+    <div id="profileExperience">
+
+      <!-- left hand column -->
+      <div  class="rellax" data-rellax-speed="4" id="aboutProfile">
+        <div id="aboutProfileImageContainer">
+          <div id="profileContactImage">
+            <p id='secPlaceholderImage'></p>      
+          </div>
+
+          <div id="profileContainerOverlay">
+          </div>
+        </div>  
+
+        <div id="profile"> 
+        <h3>profile</h3>
+          <p>I started to re-train as a web developer in 2020 when Covid caused my current job as a furniture maker/designer to cease. It allowed me to re-think my career and be a part of something that is future proof. I exercise a high attention to detail to maintain accuracy and consistency in my personal performance, collaborative teamwork and on-time completion of all projects. Moreover, I offer customer service abilities and problem-solving. Although it is a career change, there are a lot of skills that I am able to transfer over. In 2017 I completed a project management course with The University of Cape Town. I have experience running large projects (from conception through to completion), large teams, working with the client to bring their ideas to life and I also understand the complexities of running a small business. </p>
+        </div>
+      </div>
+
+
+      <!-- right hand column -->
+
+      <div  class="rellax" data-rellax-speed="4" id="workExperience">
+
+        <!--work experience -->
+        <div class="subSections" >
+          <div class="subHeadings">
+            <h3>Work Experience</h3>
+            <div class="subLine"></div>
+          </div>
+          <div class="information">
+            <h4>Junior Web Developer</h4>
+            <h5>self-employed, Cape Town, Western Cape</h5>
+            <ul>
+              <li>Coded websites using HTML, CSS and JavaScript languages.</li>
+              <li>Conducted testing and review of website design for responsiveness, clarity and effectiveness.</li>
+              <li>Employed coding practices based on commonly accepted standards to establish site layout and user interface.</li>
+              <li>Built and managed Wheatley Studios website, using Shopify template.</li>
+              <li>Maintenance and updates to WordPress website for Verb Consulting.</li>
+            </ul>
+            <h4>Owner</h4>
+            <h5>Wheatley Studios, Cape Town, Western Cape</h5>
+            <p>in my spare time I run Wheatley Studios, a small furniture design studio that developed an intelligent modular shelving system.
+            It is a system that is simple, easy to create and install. We work with interior designers and resellers.
+            Some interchangeable skills that I have learned from running Wheatley Studios include.
+            </p>
+
+            <ul>
+              <li>Client management and relationship building.</li>
+              <li>Job scope and project management.</li>
+              <li>Team management.</li>
+            </ul>
+            <p>Full employment history can be found on LinkedIn.</p>
+          </div>
+        </div>
+
+        <!--education -->
+
+        <div class="subSections" id="education">
+          <div class="subHeadings">
+            <h3>Education</h3>
+            <div class="subLine"></div>
+          </div> 
+          
+          <div class="information">
+            <h4>Full Stack Web Development </h4>
+            <h5>CodeSpace - Cape Town</h5>
+            <p>"We believe that individuals need to be taught how to “learn, unlearn and relearn” in order for them to remain relevant in a rapidly changing tech landscape. Beyond our industry-relevant curriculum, learning methodologies are fostered during the course, empowering interns to continuously self-learn even after the course. The course is highly practical, involving daily coding challenges while also submitting multiple projects throughout."</p>
+            
+            <h4>Web Development</h4>
+            <h5>CodeCademy - Online</h5>
+            <p>Codecademy is an education company. But not one in the way you might think. We're committed to building the best learning experience inside and out, making Codecademy the best place for our team to learn, teach, and create the online learning experience of the future.</p>
+
+            <h4>WordPress Development</h4>
+            <h5>Udemy - Online</h5>
+            <p>The leading global marketplace for learning and instruction. By connecting students all over the world to the best instructors, Udemy is helping individuals reach their goals and pursue their dreams.</p>
+          </div>
+        </div>
+
+        <!-- language -->
+
+        <div class="subSections" id="languages"> 
+          <div class="subHeadings">
+            <h3>languages</h3>
+            <div class="subLine"></div>
+          </div>  
+        
+          <div id="languageContainer">
+            <div class="ratingContainer">
+              <h5>HTML</h5>
+                <div class="rating">
+                  <div class="skillRating rated"></div>
+                  <div class="skillRating rated"></div>
+                  <div class="skillRating rated"></div>
+                  <div class="skillRating rated"></div>
+                  <div class="skillRating"></div>
+                </div>
+              <p>This is information about HTML.</p>
+            </div>
+
+            <div class="ratingContainer">
+              <h5>CSS</h5>
+                <div class="rating">
+                  <div class="skillRating rated"></div>
+                  <div class="skillRating rated"></div>
+                  <div class="skillRating rated"></div>
+                  <div class="skillRating rated"></div>
+                  <div class="skillRating"></div>
+                </div>    
+                <p>This is information about CSS.</p>
+            </div>
+
+            <div class="ratingContainer">
+              <h5>Javascript</h5>
+                <div class="rating">
+                  <div class="skillRating rated"></div>
+                  <div class="skillRating rated"></div>
+                  <div class="skillRating rated"></div>
+                  <div class="skillRating"></div>
+                  <div class="skillRating"></div>
+                </div>
+              <p>This is information about Javascript.</p>
+            </div>
+
+            <div class="ratingContainer">
+              <h5>PHP</h5>
+                <div class="rating">
+                  <div class="skillRating rated"></div>
+                  <div class="skillRating rated"></div>
+                  <div class="skillRating rated"></div>
+                  <div class="skillRating"></div>
+                  <div class="skillRating"></div>
+                </div>
+                <p>This is information about PHPL.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>  
+
+
+
+
+    
+
+
+
+
+ 
+  </body>
+  <script src="rellax-master/rellax.min.js" type="text/javascript"></script>
+  <script src="script/script.js" type="text/javascript"></script>
+
+  <footer class="footerContainer">
+  </footer>
+
+</html>
+
